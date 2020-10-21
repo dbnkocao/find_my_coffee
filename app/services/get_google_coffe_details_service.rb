0625274be8306f0feb15bef604668c1a8d68b993
Daniel Nascimento <dbnkocao@gmail.com>
2020-10-21 11:46:19 -0300

    creating google services to get list and datails

---
+++ app/services/get_google_coffe_details_service.rb
@@ -0,0 +1,20 @@
+require "rest-client"
+require "json"
+
+class GetGoogleCoffeDetailsService
+  def initialize(google_place_id)
+    @google_place_id = google_place_id
+  end
+
+  def call
+    begin
+      key = Rails.application.credentials.google_secret_key
+      base_url = "https://maps.googleapis.com/maps/api/place/details/json?place_id=#{@google_place_id}&key=#{key}"
+      response = Restclient.get base_url
+      JSON.parse(response.body)
+    rescue RestClient::ExceptionWithResponse =>
+      e.response
+    end
+
+  end
+end
